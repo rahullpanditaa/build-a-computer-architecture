bfd5423af0d00680c3a7c77948a2c46aa17d805a
Rahul Pandita <rpandita1999@outlook.com>
2024-12-31 11:42:50 +0530

    Created program example; adds 2 numbers in R0 and R1; puts them in R2

---
+++ 04-machine-language/Add2.asm
@@ -0,0 +1,10 @@
+// Add two numbers
+// Compute: RAM[2] = RAM[0] + RAM[1]
+// Usage: Put values in RAM[0] and RAM[1]
+
+@R0         // selects M=RAM[0]
+D=M         // D=RAM[0]
+@R1         // selects M=RAM[1]
+D=D+M       // D = RAM[0] + RAM[1]
+@R2         // selects M=RAM[2]
+M=D         // M = RAM[0] + RAM[1]
