3bc35d11d37a7332d0351cbca8242d793df41ad3
Rahul Pandita <rpandita1999@outlook.com>
2025-01-11 10:41:13 +0530

    Writing contents of an array to a file

--- assembler/src/Assembler.java
+++ assembler/src/Assembler.java
@@ -6,10 +6,18 @@ public class Assembler {
 
     public static void main(String[] args) {
 
+        String[] bits = {"0000101010001110",
+                         "1000101000111001",
+                         "1111000010111001"};
         try {
             BufferedWriter writer = new BufferedWriter(
                     new FileWriter("output.hack"));
             writer.write("00001111001010010");
+            writer.write("\n1000101000101110");
+
+            for (String binaryCode : bits) {
+                writer.write("\n" + binaryCode);
+            }
             writer.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
