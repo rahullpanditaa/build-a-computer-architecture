c21d248428e80debd74edc752252e809a13eeb7c
Rahul Pandita <rpandita1999@outlook.com>
2025-01-11 09:45:43 +0530

    Created basic outline of the Parser API; no handling of symbols yet

---
+++ assembler/src/Parser.java
@@ -0,0 +1,35 @@
+public class Parser {
+
+    Parser() {
+        // open source text file
+    }
+
+    public boolean hasMoreLines() {
+        // check whether the text file opened by this Parser object has a next line
+    }
+
+    String advance() {
+        // if there is a next line or instruction, go to it and make it the current
+        // instruction
+    }
+
+    // Parsing the current instruction
+    ??? instructionType() {
+        // return the type of current instruction as a constant
+        // A instruction or C instruction
+    }
+
+    // Not dealing with symbols yet
+
+    String dest() {
+        // returns the instruction's dest field (will go to code api)
+    }
+
+    String comp() {
+        // returns the C instruction's comp field
+    }
+
+    String jump() {
+        // returns the C instruction's jump field
+    }
+}
