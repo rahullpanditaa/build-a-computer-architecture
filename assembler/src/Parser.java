35e74767c719be1029bef4d5173682d6e3ff8ab1
Rahul Pandita <rpandita1999@outlook.com>
2025-01-13 10:07:34 +0530

    Modified translate method to only translate A instructions, not C; might change

--- assembler/src/Parser.java
+++ assembler/src/Parser.java
@@ -63,9 +63,9 @@ public class Parser {
     // if current instruction type is an A instruction -> @value
     // current instruction into binary = "0" + value into 15 bit binary constant
     // write to output file Xxx.hack
-    public String translate(String currentInstruction) {
-        switch (currentInstructionType) {
-            case A_INSTRUCTION:
+    public String translateAInstruction(String currentInstruction) {
+//        switch (currentInstructionType) {
+//            case A_INSTRUCTION:
                 // stores decimal after @ in @value as a string
                 String decimalValAsString = currentInstruction.substring(1);
 //                value as an decimal int
@@ -73,50 +73,23 @@ public class Parser {
                 // binary representation of value in A instruction @value (still not padded to 15 bits)
                 String binaryString = Integer.toBinaryString(decimalVal);
                 // A instruction op-code + 15 bit binary equivalent of decimal value
-                String paddedBinaryString = "0" + String.format("%15s", binaryString).replace(" ", "0");
-                return paddedBinaryString;
-                break;
-            case C_INSTRUCTION:
+                return "0" + String.format("%15s", binaryString).replace(" ", "0");
+//            case C_INSTRUCTION:
+                // if current instruction is a C instruction;
+                // 3 possible fields -> dest=comp;jump
+                // only comp is definitely present; rest are optional
+                // need to check if dest and jump exist -> if neither exist, jump(); rest null
+//                return "j";
+//            default:
+//                return "null";
+        }
+
+
+    // dest=comp;jump
+    public String comp() {
+        if ((!currentInstruction.contains("=")) && (!(currentInstruction.contains(";")))) {
+            // if above condition passes, instruction only has comp
+            return currentInstruction;
         }
     }
-
-
-    
-    // -----
-    // if current instruction is a C instruction -> dest=comp;jump
-    //
-
-
 }
-    /*
-    public boolean hasMoreLines() {
-        // check whether the text file opened by this Parser object has a next line
-    }
-
-    String advance() {
-        // if there is a next line or instruction, go to it and make it the current
-        // instruction
-    }
-
-    // Parsing the current instruction
-    ??? instructionType() {
-        // return the type of current instruction as a constant
-        // A instruction or C instruction
-        // probably enums
-    }
-
-    // Not dealing with symbols yet
-
-    String dest() {
-        // returns the instruction's dest field (will go to code api)
-    }
-
-    String comp() {
-        // returns the C instruction's comp field
-    }
-
-    String jump() {
-        // returns the C instruction's jump field
-    }
-}
-*/
