cb99b7728d26b31020ed0989f7f39528910e5cf4
Rahul Pandita <rpandita1999@outlook.com>
2025-01-13 08:56:09 +0530

    Created translate method; currently can convert an A instruction into equivalent 16 bit value and return it

--- assembler/src/Parser.java
+++ assembler/src/Parser.java
@@ -4,9 +4,8 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Parser {
-//    String inputAsmFileName;
+    //    String inputAsmFileName;
     BufferedReader reader;
-
 
 
     public Parser(String inputAsm) {
@@ -18,19 +17,23 @@ public class Parser {
             throw new RuntimeException(e);
         }
     }
+
     // this method should ignore whitespace and comments
     public boolean hasMoreLines() {
         // check whether the text file opened by this Parser object has a next line
         try {
             String nextLine = reader.readLine();
-            return (nextLine != null && nextLine.startsWith("//") &&nextLine.trim().isEmpty());
+            return (nextLine != null && nextLine.startsWith("//") && nextLine.trim().isEmpty());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+
     // if there is a next line or instruction, go to it and make it the current
     // instruction
+    // static field?
     String currentInstruction;
+
     public void advance() {
         if (hasMoreLines()) {
             try {
@@ -41,8 +44,9 @@ public class Parser {
         }
     }
 
-    // Parsing the current instruction
+    // Parsing the current instruction and return its instruction type
     InstructionType currentInstructionType;
+
     public InstructionType instructionType() {
         // return the type of current instruction as a constant
         // A instruction or C instruction
@@ -54,6 +58,33 @@ public class Parser {
         }
         return currentInstructionType;
     }
+
+    // TRANSLATING A AND C INSTRUCTIONS INTO BINARY CODE
+    // if current instruction type is an A instruction -> @value
+    // current instruction into binary = "0" + value into 15 bit binary constant
+    // write to output file Xxx.hack
+    public String translate(String currentInstruction) {
+        switch (currentInstructionType) {
+            case A_INSTRUCTION:
+                // stores decimal after @ in @value as a string
+                String decimalValAsString = currentInstruction.substring(1);
+//                value as an decimal int
+                int decimalVal = Integer.parseInt(decimalValAsString);
+                // binary representation of value in A instruction @value (still not padded to 15 bits)
+                String binaryString = Integer.toBinaryString(decimalVal);
+                // A instruction op-code + 15 bit binary equivalent of decimal value
+                String paddedBinaryString = "0" + String.format("%15s", binaryString).replace(" ", "0");
+                return paddedBinaryString;
+                break;
+            case C_INSTRUCTION:
+        }
+    }
+
+
+    
+    // -----
+    // if current instruction is a C instruction -> dest=comp;jump
+    //
 
 
 }
